4cc9882a07688892039f55094e39273702f2dbe5
sami janafse <goblinsami@gmail.com>
2021-12-26 20:58:09 +0100

    mobile behaviour

--- app/javascript/packs/sidebar.js
+++ app/javascript/packs/sidebar.js
@@ -1,7 +1,8 @@
-$("#menu-toggle").click(function(e) {
+$(".menu-toggle").click(function(e) {
   e.preventDefault();
   $("#wrapper").toggleClass("toggled");
   $("#open-btn").toggleClass("fa-chevron-circle-right")
+  $("#open-btn").innerHTML = "Close"
   $(".menu-item").toggleClass("disolve")
   $(".fa-user-graduate").toggleClass("d-block")
 
