066de393758b642f04f3f023ce96804c219fabc5
sami janafse <goblinsami@gmail.com>
2021-12-28 12:35:38 +0100

    set up

--- app/javascript/packs/sidebar.js
+++ app/javascript/packs/sidebar.js
@@ -2,7 +2,6 @@ $(".menu-toggle").click(function(e) {
   e.preventDefault();
   $("#wrapper").toggleClass("toggled");
   $("#open-btn").toggleClass("fa-chevron-circle-right")
-  $("#open-btn").innerHTML = "Close"
   $(".menu-item").toggleClass("disolve")
   $(".fa-user-graduate").toggleClass("d-block")
 
